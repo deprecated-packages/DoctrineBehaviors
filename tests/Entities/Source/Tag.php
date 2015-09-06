d690d491aaab4fe0d8c99a252e2dbfa763246014
Tomas Pilar <email@tomaspilar.cz>
2015-09-06 09:21:50 +0200

    add sources for testing

---
+++ tests/Entities/Source/Tag.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Zenify\DoctrineBehaviors\Tests\Entities\Source;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+
+/**
+ * @ORM\Entity
+ */
+class Tag
+{
+
+	/**
+	 * @ORM\Id
+	 * @ORM\Column(type="integer")
+	 * @ORM\GeneratedValue(strategy="IDENTITY")
+	 * @var int
+	 */
+	private $id;
+
+	/**
+	 * @ORM\Column(type="string")
+	 * @var string
+	 */
+	private $name;
+
+	/**
+	 * @ORM\ManyToMany(targetEntity="CategoryTranslation", inversedBy="tags", cascade={"persist"})
+	 * @var CategoryTranslation[]|ArrayCollection
+	 */
+	private $categories;
+
+
+	public function __construct($name)
+	{
+		$this->name = $name;
+		$this->categories = new ArrayCollection;
+	}
+
+
+	/**
+	 * @return string
+	 */
+	public function getName()
+	{
+		return $this->name;
+	}
+
+}
